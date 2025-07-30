466c8904106f61aa8dfe911836156a931a104684
Shawn <desh0wiz@gmail.com>
2025-07-29 18:08:15 -0700

    Mixed spawning with randomness

--- SurvivorLikeSelf/Assets/Scripts/Managers/SpawnManager.cs
+++ SurvivorLikeSelf/Assets/Scripts/Managers/SpawnManager.cs
@@ -1,14 +1,26 @@
+using System;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField]
     private GameObject _testEnemy = null;
+
+    [Header("Settings")]
     [SerializeField]
     private int _enemiesToSpawn = 10;
+    [SerializeField]
+    private float _xAxisSpawnSize = 2.5f;
+    [SerializeField]
+    private float _yAxisSpawnSize = 1.40625f;
 
     private void Start()
     {
+        UnityEngine.Random.InitState((int)DateTime.Now.Ticks); // Simple initialization for randomness
+        // References docs: https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Camera-orthographicSize.html
+        _yAxisSpawnSize = Camera.main.orthographicSize/* * 2f*/;
+        _xAxisSpawnSize = _yAxisSpawnSize * Camera.main.aspect;
         SpawnWave();
     }
 
@@ -22,10 +34,13 @@ public class SpawnManager : MonoBehaviour
 
         for (int i = 0; i < _enemiesToSpawn; i++)
         {
-            for (int j = 0; j < _enemiesToSpawn; j++)
-            {
-                GameObject newEnemy = Instantiate(_testEnemy, new Vector3(i, j, 0f), Quaternion.identity, transform);
-            }
+            GameObject newEnemy = Instantiate(
+                _testEnemy,
+                new Vector3(UnityEngine.Random.Range(-_xAxisSpawnSize, _xAxisSpawnSize),
+                    UnityEngine.Random.Range(-_yAxisSpawnSize, _yAxisSpawnSize), 0f),
+                Quaternion.identity,
+                transform
+                );
         }
     }
 }
