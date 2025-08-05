ed185da9ddd66fb7abc85560f2d0bdf0bc344374
Shawn <desh0wiz@gmail.com>
2025-08-04 17:06:54 -0700

    Multi weapon flipping

--- SurvivorLikeSelf/Assets/Scripts/Weapons/RangedWeapon.cs
+++ SurvivorLikeSelf/Assets/Scripts/Weapons/RangedWeapon.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class RangedWeapon : Weapon
 {
     [SerializeField]
+    private bool _onlyWeapon = true;
+    [SerializeField]
     protected SpriteRenderer _weaponSprite = null;
     [Header("Projectile")]
     [SerializeField]
@@ -17,8 +19,11 @@ public class RangedWeapon : Weapon
 
     private Vector2 _startingWeaponPosition = Vector2.zero;
 
+    private float _lastPosX = 0f;
+
     protected override void InitializeWeapon()
     {
+        _lastPosX = transform.position.x;
         _startingWeaponPosition = transform.localPosition;
         base.InitializeWeapon();
     }
@@ -39,33 +44,60 @@ public class RangedWeapon : Weapon
         Vector3 direction = _currentTargetPosition - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         _swivelTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        if (_weaponSprite.flipX)
+        if (_onlyWeapon)
         {
+            if (_weaponSprite.flipX)
+            {
+                _weaponSprite.flipX = false;
+                _weaponSprite.flipY = true;
+            }
+            float transformLocalX = transform.localPosition.x;
+            if (transformLocalX == _startingWeaponPosition.x && transform.position.x - 0.25f > _currentTargetPosition.x)
+            {
+                _weaponSprite.flipY = true;
+                transform.localPosition = new Vector3(-_startingWeaponPosition.x, _startingWeaponPosition.y, 0f);
+            }
+            else if (transformLocalX != _startingWeaponPosition.x && _currentTargetPosition.x - 0.25f > transform.position.x)
+            {
+                _weaponSprite.flipY = false;
+                transform.localPosition = _startingWeaponPosition;
+            }
+        }
+        else
+        {
+            _weaponSprite.flipY = transform.position.x >= _currentTargetPosition.x;
             _weaponSprite.flipX = false;
-            _weaponSprite.flipY = true;
         }
-        float transformLocalX = transform.localPosition.x;
-        if (transformLocalX == _startingWeaponPosition.x && transform.position.x - 0.25f > _currentTargetPosition.x)
-        {
-            _weaponSprite.flipY = true;
-            transform.localPosition = -_startingWeaponPosition;
-        }
-        else if (transformLocalX != _startingWeaponPosition.x && _currentTargetPosition.x - 0.25f > transform.position.x)
-        {
-            _weaponSprite.flipY = false;
-            transform.localPosition = _startingWeaponPosition;
-        }
+        _lastPosX = transform.position.x;
     }
 
     protected override void ResetRotation()
     {
         base.ResetRotation();
         //_weaponSprite.flipY = false;
-        if (transform.localPosition.x != _startingWeaponPosition.x)
+        if (_onlyWeapon)
+        {
+            if (transform.localPosition.x != _startingWeaponPosition.x)
+            {
+                _weaponSprite.flipY = false;
+                _weaponSprite.flipX = true;
+            }
+        }
+        else
         {
             _weaponSprite.flipY = false;
-            _weaponSprite.flipX = true;
+            if (_lastPosX <= transform.position.x)
+            {
+                _weaponSprite.flipX = false;
+                // transform.localPosition = _startingWeaponPosition;
+            }
+            else
+            {
+                _weaponSprite.flipX = true;
+                // transform.localPosition = new Vector3(-_startingWeaponPosition.x, _startingWeaponPosition.y, 0f);
+            }
         }
+        _lastPosX = transform.position.x;
     }
 
 
