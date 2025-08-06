c12df6ba087deab93411de4e2e0b7e5d8046616b
Shawn <desh0wiz@gmail.com>
2025-08-06 14:45:04 -0700

    EventManager and restart event

---
+++ SurvivorLikeSelf/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static EventManager gameManager;
+    public static EventManager instance
+    {
+        get
+        {
+            if (!gameManager)
+            {
+                gameManager = FindFirstObjectByType<EventManager>();
+
+                if (!gameManager)
+                {
+                    Debug.LogError("There needs to be one active EventManager script on a GameObject in your scene.");
+                }
+                else
+                {
+                    //gameManager.Init();
+
+                    //  Sets this to not be destroyed when reloading scene
+                    DontDestroyOnLoad(gameManager);
+                }
+            }
+            return gameManager;
+        }
+    }
+
+    void OnEnable()
+    {
+        EventManager.StartListening("gameover", Restart);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("gameover", Restart);
+    }
+
+    public void Restart(Dictionary<string, object> message)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- SurvivorLikeSelf/Assets/Scripts/PlayerBehaviors/Player.cs
+++ SurvivorLikeSelf/Assets/Scripts/PlayerBehaviors/Player.cs
@@ -1,8 +1,12 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour, IDamageable
 {
+    [Header("References")]
+    [SerializeField]
+    private SpriteRenderer _playerSprite = null;
     [Header("Stats")]
     [SerializeField]
     private float _baseMaximumHealth = 10f;
@@ -13,9 +17,14 @@ public class Player : MonoBehaviour, IDamageable
     [Header("Settings")]
     [SerializeField]
     private float _invulnTimer = 0.5f;
+    [SerializeField]
+    [Tooltip("Flash duration should be lower than invuln timer")]
+    private float _flashDuration = 0.1f;
 
     private Vector2 _currentMovementVector = Vector2.zero;
     private float _lastTimeHit = -Mathf.Infinity;
+
+    private Coroutine _flashRoutine = null;
 
     void Start()
     {
@@ -51,13 +60,46 @@ public class Player : MonoBehaviour, IDamageable
             Die();
             return;
         }
+        DamageFlash();
         //Push(incomingKnockback);
+    }
+
+    private void DamageFlash()
+    {
+        if (_flashRoutine != null)
+        {
+            StopCoroutine(_flashRoutine);
+        }
+        _flashRoutine = StartCoroutine(FlashRoutine());
+    }
+
+    private IEnumerator FlashRoutine()
+    {
+        float startTime = Time.time;
+        float progress = 0f;
+
+        while (Time.time - startTime < _flashDuration)
+        {
+            progress += Time.deltaTime;
+            _playerSprite.color = Color.Lerp(Color.white, Color.red, progress / _flashDuration);
+            yield return null;
+        }
+
+        startTime = Time.time;
+        progress = 0f;
+
+        while (Time.time - startTime < _flashDuration)
+        {
+            progress += Time.deltaTime;
+            _playerSprite.color = Color.Lerp(Color.red, Color.white, progress / _flashDuration);
+            yield return null;
+        }
+
+        _playerSprite.color = Color.white;
     }
 
     public void Die()
     {
-        Debug.Log("Dead");
-        //Destroy(gameObject);
-        Time.timeScale = 0f;
+        EventManager.TriggerEvent("gameover", null);
     }
 }
